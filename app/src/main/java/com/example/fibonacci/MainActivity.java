9c038875727d223986d05ad81f4d38267b789c9a
Christina Fernandes <fernandes.christ14@gmail.com>
2022-05-02 21:07:16 -0300

    Código para calculo de Fibonacci

--- app/src/main/java/com/example/fibonacci/MainActivity.java
+++ app/src/main/java/com/example/fibonacci/MainActivity.java
@@ -6,9 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private Button buttonCalcular;
+    private EditText userDigitar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +20,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         buttonCalcular = findViewById(R.id.btnCalcular);
+        userDigitar = findViewById(R.id.userDigitar);
 
         buttonCalcular.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), ResultadoCalculo.class);
+                Intent intent = new Intent(MainActivity.this, ResultadoCalculo.class);
+                int number1 = 0;
+                int number2 = 1;
+                int soma = 0;
+                ArrayList<Integer> arrayList = new ArrayList<Integer>();
 
+                int userDigitaNumber = Integer.parseInt(userDigitar.getText().toString());
+                arrayList.add(number1);
+                arrayList.add(number2);
+
+                for (int i = 2; i < userDigitaNumber; i++) {
+                    soma = number1 + number2;
+                    number1 = number2;
+                    number2 = soma;
+                    arrayList.add(soma);
+                }
+                intent.putExtra("resultado", arrayList);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/fibonacci/ResultadoCalculo.java
+++ app/src/main/java/com/example/fibonacci/ResultadoCalculo.java
@@ -4,12 +4,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class ResultadoCalculo extends AppCompatActivity {
     private Button buttonVoltar;
+    private TextView resultadoFinal;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -18,12 +22,17 @@ public class ResultadoCalculo extends AppCompatActivity {
 
         buttonVoltar = findViewById(R.id.btnVoltar);
 
+        Intent intent = getIntent();
+        ArrayList<Integer> dadosRecebidos = intent.getIntegerArrayListExtra("resultado");
+
+
+        resultadoFinal = findViewById(R.id.textResultado);
+        resultadoFinal.setText(String.valueOf(dadosRecebidos));
+
         buttonVoltar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-
-
                 startActivity(intent);
             }
         });
